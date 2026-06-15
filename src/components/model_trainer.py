97db60b49d74d51696945cd052cd969d7af972c2
GouravGC <chhatwanigourav@gmail.com>
2026-06-15 16:15:52 +0530

    Hyperparameter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -38,8 +38,8 @@ class ModelTrainer:
             ) 
             models =  {
                 "Linear Regression": LinearRegression(),
-                "Ridge": Ridge(),
-                "Lasso": Lasso(),
+                'Ridge' : Ridge(),
+                'Lasso' : Lasso(),
                 "KNN": KNeighborsRegressor(),
                 "Decision Tree": DecisionTreeRegressor(random_state=42),
                 "Random Forest": RandomForestRegressor(random_state=42),
@@ -49,9 +49,52 @@ class ModelTrainer:
                 "CatBoost": CatBoostRegressor(verbose=0, random_state=42),
                 "GradientBoost":GradientBoostingRegressor()
             }
+            params={  
+                "Linear Regression": {},
+                "Ridge": {},
+                "Lasso": {},
+                "KNN": {},
+                "SVR":{},
+
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "GradientBoost":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "XGBoost":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoost":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
 
             model_report : dict = evaluate_models(X_train = X_train,y_train = y_train,X_test = X_test , y_test = y_test,
-                                                 models = models)
+                                                 models = models,param = params)
             
             ## To get the best model score from dict
             best_model_score = max(sorted(model_report.values()))
